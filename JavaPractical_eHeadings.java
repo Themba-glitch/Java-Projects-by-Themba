0281a2cbd1127edf15e8cd28e0b7e477be789922
Themba <sefobongani19@gmail.com>
2026-01-28 11:46:30 +0200

    Add files via upload

---
+++ JavaPractical_eHeadings.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javapractical_eheadings;
+
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class JavaPractical_eHeadings {
+
+    public static void main(String[] args) {
+      
+    }
+} 
